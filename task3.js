bb15f73d2792c5a81c415fa9a254f82ce5a075e8
Jayant Kumar Naik <jayantnaik98@gmail.com>
2022-06-16 18:19:55 +0530

    first commit

---
+++ task3.js
@@ -0,0 +1,20 @@
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function clearScreen()
+{
+    document.getElementById("result").value = "";
+}
+   
+function calculate()
+{
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function del()
+{
+    result.value = result.value.slice(0, -1);
+}
